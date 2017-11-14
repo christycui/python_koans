0186f2a5e5ba08dda58828acd572387300a29dee
christycui <christycui14@gmail.com>
2017-11-13 21:08:04 -0500

    more

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,22 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    n_unique_sides = len({a, b, c})
+    # all sides should be greater than zero
+    if min({a, b, c}) <= 0:
+      raise TriangleError
+
+    # sum of two sides should be greater than the third one
+    sorted_sides = sorted([a, b, c])
+    if sum(sorted_sides[:2]) <= sorted_sides[-1]:
+      raise TriangleError
+
+    if n_unique_sides == 1:
+      return 'equilateral'
+    elif n_unique_sides == 2:
+      return 'isosceles'
+    else:
+      return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
